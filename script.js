08838f98c3d77d15b5efa790a24b90cfcbdcdc21
secredasho <146038531+secredasho@users.noreply.github.com>
2024-02-24 17:49:46 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-function games() {
-  window.location.replace("https://planck-v1.vercel.app/games.html")
-}
-function home() {
-  window.location.replace("https://planck-v1.vercel.app/index.html")
-}
+
