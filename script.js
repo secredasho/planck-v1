7ff43fad62df496c374deeac110444178f7c8788
secredasho <146038531+secredasho@users.noreply.github.com>
2024-02-24 19:24:04 -0600

    fix class div

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-let windowValue = window.location
-function games() {
-  windowValue += "/games.html"
-  console.log(windowValue)
+function createIframe() {
+  var win = window.open("about:blank", "Iframe Window");
+  win.document.write(`
+    <iframe src="https://images-opensocial.googleusercontent.com/gadgets/ifr?url=https://427396048-642845047394716217.preview.editmysite.com/uploads/b/139890129-761103484729797659/files/ovof.xml" style="width:100%; height:100%; border:none;"></iframe>
+  `);
 }
